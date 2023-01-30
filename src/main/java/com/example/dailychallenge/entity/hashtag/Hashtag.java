26df6c14e2ca5fd69f47173ec52d86efbda4b5f8
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-01-30 14:42:07 +0900

    refactor(HashTag): challengeHashtags에 지연 로딩 추가

--- src/main/java/com/example/dailychallenge/entity/hashtag/Hashtag.java
+++ src/main/java/com/example/dailychallenge/entity/hashtag/Hashtag.java
@@ -1,12 +1,19 @@
 package com.example.dailychallenge.entity.hashtag;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Table(name = "hashtag")
 @Entity
@@ -23,7 +30,7 @@ public class Hashtag {
 
     private Integer tagCount;
 
-    @OneToMany(mappedBy = "hashtag",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "hashtag", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<ChallengeHashtag> challengeHashtags= new ArrayList<>();
 
     @Builder
