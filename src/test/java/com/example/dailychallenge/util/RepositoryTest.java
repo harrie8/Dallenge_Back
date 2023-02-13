9619588315b3a2d4a340cd7473b415a5aba7c82d
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-02-13 19:16:27 +0900

    refactor: repository 테스트 실행할 때 application-test.properties 사용하도록 변경

--- src/test/java/com/example/dailychallenge/util/RepositoryTest.java
+++ src/test/java/com/example/dailychallenge/util/RepositoryTest.java
@@ -1,10 +1,12 @@
 package com.example.dailychallenge.util;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
 //@Import(JPAConfig.class)
 //@Configuration
 //@EnableJpaAuditing
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class RepositoryTest {
 }
