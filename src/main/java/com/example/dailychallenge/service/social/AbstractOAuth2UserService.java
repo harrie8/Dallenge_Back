02c0e017ecc37f593760ee5e1614d8bf6d69b2a3
dlask913 <dlask913@inu.ac.kr>
2023-03-29 22:54:00 +0900

    feat: 카카오 소셜 로그인 구현

--- src/main/java/com/example/dailychallenge/service/social/AbstractOAuth2UserService.java
+++ src/main/java/com/example/dailychallenge/service/social/AbstractOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.example.dailychallenge.service.social;
 
 import com.example.dailychallenge.entity.social.GoogleUser;
+import com.example.dailychallenge.entity.social.KakaoUser;
 import com.example.dailychallenge.entity.social.ProviderUser;
 import com.example.dailychallenge.entity.users.User;
 import com.example.dailychallenge.repository.UserRepository;
@@ -11,6 +12,7 @@ import java.util.Optional;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -45,6 +47,8 @@ public abstract class AbstractOAuth2UserService {
 
         if (registrationId.equals("google")) { // 소셜 로그인 중 google 인 경우
             return new GoogleUser(oAuth2User, clientRegistration);
+        } else if (registrationId.equals("kakao")) {
+            return new KakaoUser(oAuth2User, clientRegistration);
         }
         return null;
     }
--- src/main/java/com/example/dailychallenge/service/social/CustomOAuth2UserService.java
+++ src/main/java/com/example/dailychallenge/service/social/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.example.dailychallenge.service.social;
 
 import com.example.dailychallenge.entity.social.ProviderUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -9,9 +10,10 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CustomOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
+@Service
+@Slf4j
+public class CustomOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User>  {
     @Override // 첨에 이리로 들어옵니다
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
@@ -19,7 +21,6 @@ public class CustomOAuth2UserService extends AbstractOAuth2UserService implement
         OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
         // 전달받은 사용자 정보 서비스에 저장
         OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
-
         ProviderUser providerUser = super.providerUser(clientRegistration, oAuth2User);
         super.register(providerUser,userRequest); // 회원가입
 
